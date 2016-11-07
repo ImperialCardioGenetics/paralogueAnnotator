bc8f1d0876b7e8986503d6b4f81e635a913c021c
Erica Mazaika <ericamazaika@Ericas-Air.vodafone>
2016-11-07 02:42:33 +0000

    Enter genomic coords of SNP to retrieve variants within equivalent codon of each paralog

--- paralogannotator.pl
+++ paralogannotator.pl
@@ -25,14 +25,16 @@ my $comparadb= new Bio::EnsEMBL::Compara::DBSQL::DBAdaptor(
 );
 
 print "Get Adaptors\n"; #(Species, Type of DB, Type of Object)
-my $hg_adaptor = $reg->get_adaptor("human","core","Gene"); 
-my $familyA = $reg->get_adaptor("Multi", "compara", "Family");
-my $seqmemberA = $reg->get_adaptor("Multi", "compara", "SeqMember");
-my $slice_adaptor = $reg->get_adaptor("Human", "core", "Slice");
-my $transcript_adaptor = $reg->get_adaptor("Human", "core", "Transcript");
-my $vf_adaptor = $reg->get_adaptor('human', 'variation', 'variationfeature');
-my $pa = $reg->get_adaptor("human","variation","phenotype");
-
+my $hg_adaptor = $reg->get_adaptor("Human","Core","Gene"); 
+my $familyA = $reg->get_adaptor("Multi", "Compara", "Family");
+my $seqmemberA = $reg->get_adaptor("Multi", "Compara", "SeqMember");
+my $slice_adaptor = $reg->get_adaptor("Human", "Core", "Slice");
+my $transcript_adaptor = $reg->get_adaptor("Human", "Core", "Transcript");
+my $vf_adaptor = $reg->get_adaptor("Human", "Variation", "Variationfeature");
+my $pa = $reg->get_adaptor("Human","Variation","Phenotype");
+my $gene_member_adaptor = Bio::EnsEMBL::Registry->get_adaptor("Multi", "Compara", "GeneMember");
+my $homology_adaptor = Bio::EnsEMBL::Registry->get_adaptor("Multi", "Compara", "Homology");
+my $gene_member_adaptor = Bio::EnsEMBL::Registry->get_adaptor("Multi", "Compara", "GeneMember");
 
 print "Enter chromosome of variant: ";
 my $chr_input = <STDIN>;
@@ -47,196 +49,137 @@ my $all_genes = $slice1->get_all_Genes;
 
 foreach $gene ( @{$all_genes} ) {
     print "Variant location within " . $gene->external_name . " " . $gene->display_id . "\n";
-    $seq1Pid = $gene->display_id;
+    $base_Gid = $gene->display_id;
+    $basegene = $gene->external_name;
 }
 
-my $gene_member_adaptor = Bio::EnsEMBL::Registry->get_adaptor('Multi', 'compara', 'GeneMember');
-my $gene_member = $gene_member_adaptor->fetch_by_stable_id($seq1Pid);
-
-
-my $homology_adaptor = Bio::EnsEMBL::Registry->get_adaptor('Multi', 'compara', 'Homology');
+my $gene_member = $gene_member_adaptor->fetch_by_stable_id($base_Gid);
 my $homologies = $homology_adaptor->fetch_all_by_Member($gene_member);
 
-my $n = 0;
-
 foreach my $homology (@{$homologies}) {
+	
+	
+	my @members = (@{$homology->get_all_Members});
+	my $para_gene;	
+	
+	#Define hashes
+	my %ENSPid = ();
+	my %ENSTid = ();
+	my %genename = ();
+	my %geneobj = ();
+	my %transslice = (); 
+	my %strand = (); 
+	my %fullseq = (); 
+	my %trmapper = (); 
+	my %peptide = ();
+	my %peptide_start = ();
+
+	
+	#Are both members human?	
+	my $hgmembercount = 0;
+	foreach my $member (@members) {
+		if ($member->taxon_id eq "9606"){
+			$hgmembercount = $hgmembercount + 1;
+		}
+	}
+
+	#If both members are human 		
+	if ($hgmembercount == 2){
 		
-		my $count = 0;
-		my $counthuman = 0;
-		my @members = (@{$homology->get_all_Members});
+		my $simplealign = $homology->get_SimpleAlign(); 
+		print $homology->description,": ";
 		
 		foreach my $example (@members) {
-			$count = $count+1;
-			my $taxon = $example->taxon_id;
-				if ($taxon eq "9606") {
-					$counthuman = $counthuman+1;
-				}
+
+			my $ENSP = $example->stable_id;
+			my $gene = $hg_adaptor->fetch_by_translation_stable_id($ENSP);
+			if ($gene->external_name eq $basegene){
+				$ENSPid{$basegene}=$example->stable_id; #confirm using longest trans for all
+				$geneobj{$basegene}=$hg_adaptor->fetch_by_translation_stable_id($ENSP);
+				$genename{$basegene}=$gene->external_name;
+				$trans{$basegene}=$example->get_Transcript;
+				$ENSTid{$basegene} = $trans{$basegene}->display_id;
+			} else {
+				$para_gene=$gene->external_name;
+				$ENSPid{$para_gene}=$example->stable_id;
+				$geneobj{$para_gene}=$hg_adaptor->fetch_by_translation_stable_id($ENSP);
+				$genename{$para_gene}=$gene->external_name;
+				$trans{$para_gene}=$example->get_Transcript;
+				$ENSTid{$para_gene} = $trans{$para_gene}->display_id;			
+			}
+	
+		}
+		print "$genename{$para_gene} \n";
+
+		$transslice{$basegene} = $slice_adaptor->fetch_by_transcript_stable_id($ENSTid{$basegene}); 
+		$strand{$basegene} = $transslice{$basegene}->strand; 
+		
+		$trmapper{$basegene} = Bio::EnsEMBL::TranscriptMapper->new($trans{$basegene});   
+		$trmapper{$para_gene} = Bio::EnsEMBL::TranscriptMapper->new($trans{$para_gene});  
+		
+		$fullseq{$basegene} = $simplealign->get_seq_by_id($ENSPid{$basegene});
+		$fullseq{$para_gene} = $simplealign->get_seq_by_id($ENSPid{$para_gene});
+
+		my %peptide_coord; 
+		my @coordlist = $trmapper{$basegene}->genomic2pep($bp_input, $bp_input, $strand{$basegene}); 
+		foreach my $var (@coordlist){
+			$peptide_coord{$basegene} = $var; 
+		}
+	
+		$peptide{$basegene} = $peptide_coord{$basegene}->start;
+		my $col = $simplealign->column_from_residue_number($ENSPid{$basegene}, $peptide{$basegene});
+
+		$peptide_coord{$para_gene} = $fullseq{$para_gene}->location_from_column($col);
+		$peptide{$para_gene} = $peptide_coord{$para_gene}->start;
+
+		if (defined $peptide{$para_gene}) { #how to deal with undefined?
+			# print "$ENSPid{$basegene}-$peptide{$basegene} ~ $ENSPid{$para_gene}-$peptide{$para_gene}\n";
+		} else {
+			# print "$ENSPid{$basegene}-$peptide{$basegene} ~ Position - in $ENSPid{$para_gene}\n";
 		}
 
-		if ($count==$counthuman){
-			print $n," ", $homology->description,"\n";
-			foreach my $example (@members) {
-				my $ENSP = $example->stable_id; #confirm using longest trans for all
-				my $gene = $hg_adaptor->fetch_by_translation_stable_id($ENSP);
-				my $trans = $example->get_Transcript;
-				print "\t" . $gene->external_name . " " . $example->stable_id . "\n";
+		my %codoncoords;
+		my @pos2bp_coords = $trmapper{$para_gene}->pep2genomic($peptide{$para_gene},$peptide{$para_gene});
+		foreach my $var (@pos2bp_coords){
+			$codoncoords{$basegene} = $var;
+		}
+		
+		foreach my $seq ($simplealign->each_seq) {
+			my $seqatlocation = $seq->subseq($col, $col);
+			if ($seq->display_id eq $ENSPid{$basegene}) {
+				print $genename{$basegene}, "\t", $ENSPid{$basegene}, "-", $peptide{$basegene}, ": ", $seqatlocation, "\n";
+			} else {
+				print $genename{$para_gene}, "\t", $ENSPid{$para_gene}, "-", $peptide{$para_gene}, ": ", $seqatlocation, "\n"
 			}
 		}
-			
-		$count = 0;
-		$counthuman = 0;
+		
+		print "\n";
 
-$n = $n+1;
-}
+		#***********************************# add filters below
+		
+		my $codon_start = $codoncoords{$basegene}->start;
+		my $codon_end = $codoncoords{$basegene}->end;
 
-print "Which homology (enter number) \n"; 
-my $homology_input = <STDIN>; 
-chomp($homology_input); 
-
-my $selectedhomology = $homologies->[$homology_input]; 
-my $simplealign = $selectedhomology->get_SimpleAlign();
-my $simplealignCDS = $selectedhomology->get_SimpleAlign(-seq_type => 'cds');
-
-my @selmembers = (@{$selectedhomology->get_all_Members});
-
-foreach my $example (@selmembers) {
-				my $ENSP = $example->stable_id;
-				my $gene = $hg_adaptor->fetch_by_translation_stable_id($ENSP);
-				my $trans = $example->get_Transcript;
-				print "\t" . $gene->external_name . " " . $example->stable_id . "\n";
-}
-
-my $alncount = 0;
-my $seq1Pid;
-my $seq2Pid;
-my $gene1;
-my $gene2;
-my $trans1;
-my $trans2;
-my $seq1Tid;
-my $member1Pid;
-my $member2Pid;
-my $member1Tid;
-my $member2Tid;
-
-my $membercount = 0;
-
-my @selmembers = (@{$selectedhomology->get_all_Members});
-foreach my $selmember (@selmembers) {
-	$membercount=$membercount+1;
-	if ($membercount==1){
-		$member1Pid = $selmember->stable_id;
-		$gene1 = $hg_adaptor->fetch_by_translation_stable_id($member1Pid);
-		$trans1 = $selmember->get_Transcript;
-		$member1Tid = $trans1->display_id;
-		$trans1b = $transcript_adaptor->fetch_by_stable_id($member1Tid);
-	}
-	if ($membercount==2){
-		$trans2 = $selmember->get_Transcript;
-		$member2Tid = $trans2->display_id;
-		$member2Pid = $selmember->stable_id;
-		$gene2 = $hg_adaptor->fetch_by_translation_stable_id($member2Pid);
-	}
-}
-
-my $slice1 = $slice_adaptor->fetch_by_transcript_stable_id($member1Tid);
-my $strand1 = $slice1->strand;
-
-print $gene1->external_name . " transcript id: $member1Tid, peptide id: $member1Pid, strand: $strand1\n";
-print $gene2->external_name . " transcript id: $member2Tid, peptide id: $member2Pid \n";
-
-foreach $seq ( $simplealignCDS->each_seq() ) {
-	$alncount=$alncount+1;
-	if ($alncount==1){
-		$seq1Pid=$seq->id;
-	}
-	if ($alncount==2){
-		$seq2Pid=$seq->id;
-	}
-}
-
-if ($seq1Pid eq $member1Pid && $seq2Pid eq $member2Pid){
-	print "Alignment order matches member order.\n\n";
-} 
-
-
-my $fullseq1 = $simplealign->get_seq_by_id($seq1Pid);
-my $fullseq2 = $simplealign->get_seq_by_id($seq2Pid);
-
-my $trmapper1 = Bio::EnsEMBL::TranscriptMapper->new($trans1);
-my $trmapper2 = Bio::EnsEMBL::TranscriptMapper->new($trans2);
-
-my $coordhash1;
-
-my @coordlist1 = $trmapper1->genomic2pep($bp_input, $bp_input, $strand1); 
-	foreach my $var1 (@coordlist1){
-		$coordhash1 = $var1;
-	}
-
-my $pos1pep = $coordhash1->start;
-
-my $col = $simplealign->column_from_residue_number($seq1Pid, $pos1pep);
-
-$pos2pep = $fullseq2->location_from_column($col);
-
-if (defined $pos2pep){
-	print "Peptide position $pos1pep in $seq1Pid : Column $col of alignment : Peptide position " . $pos2pep->start . " in $seq2Pid\n";
-} else {
-	print "Position $pos1pep in $seq1Pid : Column $col of alignment : Position - in $seq2Pid\n";
-}
-
-my $pos2pep_start = $pos2pep->start;
-
-
-my $coordhash;
-my @pos2bp_coords = $trmapper2->pep2genomic($pos2pep_start,$pos2pep_start);
-foreach my $var (@pos2bp_coords){
-	$coordhash = $var;
-	#print $coordhash;
+		$transslice{$para_gene} = $slice_adaptor->fetch_by_transcript_stable_id($ENSTid{$para_gene});
+		my $coord_sys2  = $transslice{$para_gene}->coord_system()->name();
+		my $slice2_chr = $transslice{$para_gene}->seq_region_name();
 
 
 
-}
-
-my $slice2 = $slice_adaptor->fetch_by_transcript_stable_id($member2Tid);
-my $coord_sys2  = $slice2->coord_system()->name();
-my $slice2_chr = $slice2->seq_region_name();
-
-my $codon_start = $coordhash->start;
-my $codon_end = $coordhash->end;
+		my $codon_slice2 = $slice_adaptor->fetch_by_region('chromosome', $slice2_chr, $codon_start, $codon_end);
+		foreach my $vf ( @{ $vf_adaptor->fetch_all_by_Slice($codon_slice2) } ) {
+			print $slice2_chr, ":", $vf->seq_region_start(), ' ', $vf->allele_string(), ' ', $vf->variation_name(), ' ', $vf->display_consequence, ' ';
+			my $id = $vf->variation_name();
+			my @csstates = @{$vf->get_all_clinical_significance_states};
+			foreach my $sig ( @csstates ) {
+				if ($sig =~ m/pathogenic/) {
+					print "SIG: " , $sig, ' ';
+				}
+			}
+		}
 	
-print "Genomic coords of homologous codon in $seq2Pid: " . $coord_sys2 . " " . $slice2_chr . ":" . $coordhash->start . "-" . $coordhash->end . "\n";	
-	
-my %count;
-foreach my $seq ($simplealign->each_seq) {
-   	my $seqatlocation = $seq->subseq($col, $col);
-   	$count{$seqatlocation}++;
-   	my $name = $seq->display_id;
-   	print $name,": ", $seqatlocation, "\n";
-}
-
-#foreach $res (keys %count) {
-#	printf "$res: $count{$res}\n";
-#}	
-
-print "\n";
-
-my $codon_slice2 = $slice_adaptor->fetch_by_region('chromosome', $slice2_chr, $codon_start, $codon_end);
-foreach my $vf ( @{ $vf_adaptor->fetch_all_by_Slice($codon_slice2) } ) {
-    print $slice2_chr, ":", $vf->seq_region_start(), ' ', $vf->allele_string(), ' ', $vf->variation_name(), ' ', $vf->display_consequence, ' ';
-	my $id = $vf->variation_name();
-	my @csstates = @{$vf->get_all_clinical_significance_states};
-	foreach my $sig ( @csstates ) {
-		print "SIG: " , $sig, ' ';
+		print "\n\n************************\n"
 	}
-	print "\n";
-#		my @csstates = @{$vf->get_all_clinical_significance_states};
-#		foreach my $sig ( @csstates ) {
-#			print $sig, ' ';
-#		}	
-	
- }
- print "\n";
- 
- 
-#print $codon_slice2->clinical_significance;
+}	
+		
+		
