4dc9fe160315fe7f813a460fea835bed83bb2d9e
Mikyung Jang <m.jang@rbht.nhs.uk>
2019-09-06 15:49:38 +0100

    use old syntax adaptor

--- ParalogueAnnotation.pm
+++ ParalogueAnnotation.pm
@@ -14,6 +14,10 @@ use Bio::EnsEMBL::Registry;
 Bio::EnsEMBL::Registry->set_reconnect_when_lost(1);
 use Bio::LocatableSeq;
 use Bio::EnsEMBL::TranscriptMapper;	
+use Bio::EnsEMBL::Utils::ConfigRegistry;
+use Bio::EnsEMBL::DBSQL::DBAdaptor;
+use Bio::EnsEMBL::Variation::DBSQL::DBAdaptor;
+use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
 
 
 use base qw(Bio::EnsEMBL::Variation::Utils::BaseVepPlugin);
@@ -53,6 +57,47 @@ sub new {
 	$config->{port} = 3306;
 	$config->{db_version} = 93;
 
+    my $dbCore = Bio::EnsEMBL::DBSQL::DBAdaptor->new(
+                    '-host'    => $config{host},
+                    '-user'    => $config{user},
+                    '-pass'    => $config{password},
+                    '-port'    => $config{port},
+                    '-species' => 'Homo sapiens',
+                    '-group'   => 'core',
+                    '-dbname' => 'homo_sapiens_core_93_38');
+
+    my @aliases = ( 'H_Sapiens', 'homo sapiens', 'Homo sapiens',
+             'Homo_Sapiens', 'Homo_sapiens', 'Homo', 'homo',
+             'human' );
+
+    Bio::EnsEMBL::Utils::ConfigRegistry->add_alias(
+                                           '-species' => 'Homo sapiens',
+                                           '-alias'   => [@aliases] );
+
+    my $dbVar = Bio::EnsEMBL::Variation::DBSQL::DBAdaptor->new(
+    				-host => $config{host},
+    				-user => $config{user},
+    				-pass => $config{password},
+    				-port => $config{port},
+    				-species => 'human',
+    				-group => 'variation',
+    				-dbname => 'homo_sapiens_variation_93_38');
+
+#
+#   my $dbCmp = Bio::EnsEMBL::Compara::DBSQL::DBAdaptor->new(
+#                   '-host' => $config{host},
+#                   '-user' => $config{user},
+#                   '-pass' => $config{password},
+#                   '-port' => $config{port},
+#                   '-species' => 'compara93',
+#                   '-dbname' => 'ensembl_compara_93');
+
+#   @aliases = ( 'ensembl_compara_93', 'compara93', 'Compara', 'compara' );
+
+#   Bio::EnsEMBL::Utils::ConfigRegistry->add_alias(
+#                                             '-species' => 'Compara93',
+#                                             '-alias'   => [@aliases]);
+
    	my $reg = 'Bio::EnsEMBL::Registry';
 
     if($config->{host}) {
@@ -69,11 +114,11 @@ sub new {
 #COMMENT ENSEMBL: cache adaptors and save some time by no regenerating them
 
 	$self->{config}->{genome_db_adaptor} = $reg->get_adaptor('Multi', 'compara', 'GenomeDB');
-	$self->{config}->{hg_adaptor} = $reg->get_adaptor("human","Core","Gene"); 
-	$self->{config}->{slice_adaptor} = $reg->get_adaptor("human", "Core", "Slice");
-	$self->{config}->{transcript_adaptor} = $reg->get_adaptor("human", "Core", "Transcript");
-  	$self->{config}->{variationfeature_adaptor} = $reg->get_adaptor("human", "Variation", "Variationfeature");
-	$self->{config}->{transcriptvariation_adaptor} = $reg->get_adaptor("human", "Variation", "TranscriptVariation");
+	$self->{config}->{hg_adaptor} = $dbCore->get_GeneAdaptor;
+	$self->{config}->{slice_adaptor} = $dbCore->get_SliceAdaptor();
+	$self->{config}->{transcript_adaptor} = $dbCore->get_TranscriptAdaptor();
+  	$self->{config}->{variationfeature_adaptor} = $dbVar->get_VariationFeatureAdaptor();
+	$self->{config}->{transcriptvariation_adaptor} = $dbVar->get_TranscriptVariationAdaptor;
 	$self->{config}->{genemember_adaptor} = $reg->get_adaptor("Multi", "compara", "GeneMember");
 	$self->{config}->{homology_adaptor} = $reg->get_adaptor('Multi', 'compara', 'Homology');
     return $self;
